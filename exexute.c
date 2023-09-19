74f51a2d5e8f5d93cad08a029627b8058b6cac08
Fredrick-Gyasi <0719000088@ttu.edu.gh>
2023-09-19 14:23:12 +0000

    simple shell

--- exexute.c
+++ exexute.c
@@ -1,46 +1,41 @@
 #include "shell.h"
 /**
- * execute_cmd - Execute a command with arguments in a child process.
- * @command: The command to execute.
+ * execute_cmd - Executes a command.
+ * @input: The command to execute.
  */
-void execute_cmd(const char *command) {
-pid_t pid;
-char *token;
-char *args[100];  /* Adjust the size as needed for your use case */
-int i = 0;
-token = strtok(command, " ");
-while (token != NULL) {
-args[i++] = token;
-token = strtok(NULL, " ");
-}
-args[i] = NULL;  /* Null-terminate the args array */
-pid = fork();
-if (pid == -1)
+void execute_cmd(char *input)
+{
+pid_t child_pid;
+int status;
+
+child_pid = fork();
+
+if (child_pid == -1)
 {
 perror("fork");
 exit(EXIT_FAILURE);
-} else if (pid == 0)
+}
+else if (child_pid == 0)
 {
-char *path = getenv("PATH");
-char *dir = strtok(path, ":");
-while (dir != NULL)
+char *args[20];
+int arg_count = 0;
+
+char *token = strtok(input, " ");
+while (token != NULL)
 {
-char cmd_path[1024];  /* Adjust the size as needed */
-snprintf(cmd_path, sizeof(cmd_path), "%s/%s", dir, args[0]);
-if (access(cmd_path, X_OK) == 0)
-{
-if (execve(cmd_path, args, NULL) == -1)
+args[arg_count++] = token;
+token = strtok(NULL, " ");
+}
+
+args[arg_count] = NULL;
+if (execve(args[0], args, NULL) == -1)
 {
 perror("execve");
 exit(EXIT_FAILURE);
 }
 }
-dir = strtok(NULL, ":");
-}
-/* If we reach here, the command was not found */
-fprintf(stderr, "Command not found: %s\n", args[0]);
-exit(EXIT_FAILURE);
-} else {
-wait(NULL);
+else
+{
+wait(&status);
 }
 }
