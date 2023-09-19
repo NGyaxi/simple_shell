d4422e9ff787c7e705e3984bc74344331dc04f20
root <fredrickgyasi@gmail.com>
2023-09-19 04:16:46 -0700

    simple shell

--- execute.c
+++ execute.c
@@ -1,32 +1,24 @@
 #include "shell.h"
 #include <sys/wait.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include <stdlib.h>
-
-extern char **environ;
-
 /**
- * execute_cmd - Execute a command using fork and execve
- * @command: The command to execute
- *
- * This function creates a child process using fork and executes
- * the specified command in the child process using execve.
- *
- * Return: None
+ * execute_cmd - Execute a command in a child process.
+ * @command: The command to execute.
  */
 void execute_cmd(const char *command)
 {
 pid_t kid_pid = fork();
-if (kid_pid == -1) {
+if (kid_pid == -1)
+{
 perror("fork");
 exit(EXIT_FAILURE);
 }
 else if (kid_pid == 0)
 {
-execve(command, (char *const *)command, environ);
+if (execlp(command, command, (char *)NULL) == -1)
+{
 perror("execve");
 exit(EXIT_FAILURE);
+}
 }
 else
 {
